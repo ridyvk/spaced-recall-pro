f1f95c2e375221c1a1e77b56d8a9cf31ab84a600
ridyvk <kamiridy@gmail.com>
2025-08-08 13:57:41 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'srp-v8-1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone())).catch(()=>{});
+        return res;
+      }).catch(()=> cached);
+      return cached || fetchPromise;
+    })
+  );
+});
