e2a93c74721e98558441cd413c891ba9c18aa8f3
ridyvk <kamiridy@gmail.com>
2025-08-08 15:04:00 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+// ==== Spaced Recall Pro service worker ====
+const CACHE = 'srp-v8-3';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 初回インストール：必要ファイルをキャッシュ
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+// 古いキャッシュの掃除
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// ネット優先＋フォールバックでキャッシュ
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone())).catch(()=>{});
+        return res;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
